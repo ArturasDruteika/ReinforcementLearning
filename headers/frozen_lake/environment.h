f9386009ecf0366aa1e8b1abce19273260f154d3
Arturas <arturas0000@gmail.com>
2022-07-06 20:04:43 +0300

    Created proto environment for frozen_lake

--- headers/frozen_lake/environment.h
+++ headers/frozen_lake/environment.h
@@ -1,10 +1,14 @@
 #pragma once
 
+#include <tuple>
 #include <vector>
 
 
 class FrozenLake
     {
+
+    private:
+        static void checkIfValidAction(int action);
 
     public:
         FrozenLake();
@@ -12,8 +16,13 @@ class FrozenLake
         int observationSpace;
         int actionSpace;
         bool isSlippery;
+        double transitionProbability;
+        int positionOnGrid;
 
         std::vector<std::vector<std::string>> env;
+
+        // 14 0.0 False {'prob': 0.3333333333333333}
+        std::tuple<int, double, bool> step(int action);
 
         ~FrozenLake();
     };
--- src/frozen_lake/environment.cpp
+++ src/frozen_lake/environment.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <vector>
 #include <string>
 
@@ -9,11 +10,35 @@ FrozenLake::FrozenLake()
     observationSpace = 4;
     actionSpace = 4;
     isSlippery = false;
+    transitionProbability = 33.3333;
+    positionOnGrid = 0;
 
     env = {{"s", "f", "f", "f"},
            {"f", "h", "f", "h"},
            {"f", "f", "f", "h"},
            {"h", "f", "f", "g"}};
 };
+
+void FrozenLake::checkIfValidAction(int action)
+{
+    std::cout << action <<" action is valid" << std::endl;
+}
+
+std::tuple<int, double, bool> FrozenLake::step(int action)
+{
+    if (action == 0)
+    {
+        FrozenLake::checkIfValidAction(action);
+    } else if (action == 1)
+    {
+        FrozenLake::checkIfValidAction(action);
+    } else if (action == 2)
+    {
+        FrozenLake::checkIfValidAction(action);
+    } else
+    {
+        FrozenLake::checkIfValidAction(action);
+    }
+}
 
 FrozenLake::~FrozenLake() = default;
