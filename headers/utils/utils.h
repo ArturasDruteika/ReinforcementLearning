9978d78a27b6b2a70d1a3b028e233746357eae8e
Arturas <arturas0000@gmail.com>
2022-07-06 21:22:57 +0300

    Added utils files

---
+++ headers/utils/utils.h
@@ -0,0 +1,9 @@
+#pragma once
+
+
+#include <vector>
+#include <string>
+
+
+[[maybe_unused]] void printMatrixContent(std::vector<std::vector<std::string>> inputMatrix);
+[[maybe_unused]] void printMatrixSize(int input);
---
+++ src/utils/utils.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "../../headers/utils/utils.h"
+
+
+[[maybe_unused]] void printMatrixContent(std::vector<std::vector<std::string>> inputMatrix)
+{
+    for (int i = 0; i < inputMatrix.size(); i++)
+    {
+        for (int j = 0; j < inputMatrix[0].size(); j++)
+        {
+            std::cout << inputMatrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl;
+}
+
+
+[[maybe_unused]] void printMatrixSize(int input)
+{
+    std::cout << input << std::endl;
+}
